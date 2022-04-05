8f88a442cf0e271dd9a0a3bda669c95c81f32461
heostoan <henrrytorresandonaire@gmail.com>
2022-04-05 13:22:04 -0500

    Animación juego

--- index.js
+++ index.js
@@ -1,26 +1,108 @@
-var posicion_ficha = 0;
-var paso = $(".paso").outerWidth();
-var total_avance = 0;
-var salida_juego = false;
+var posicion_ficha_arriba = 0;
+var posicion_ficha_derecha = 0;
+var posicion_ficha_izquierda = 0;
+var posicion_ficha_abajo = 0;
+var paso_ancho = $(".paso").outerWidth();
+var paso_alto = $(".paso").outerHeight();
+var total_avance_arriba = 0;
+var total_avance_derecha = 0;
+var total_avance_izquierda = 0;
+var total_avance_abajo = 0;
+var salida_juego = false;//pasar a false
+var valor_dado = 0;
+var acumulador_valor_dados = 0;
+
 function lanzar_dado() {
-    return Math.floor(Math.random() * 6) + 1;
-    //return 1;
+    var resultado_dado = Math.floor(Math.random() * 6) + 1;
+    //var resultado_dado = 1;
+    if (resultado_dado === 1) {
+        $(".jugar").css("background-image", "url(img/1.jpg)");
+    }
+    if (resultado_dado === 2) {
+        $(".jugar").css("background-image", "url(img/2.jpg)");
+    }
+    if (resultado_dado === 3) {
+        $(".jugar").css("background-image", "url(img/3.jpg)");
+    }
+    if (resultado_dado === 4) {
+        $(".jugar").css("background-image", "url(img/4.jpg)");
+    }
+    if (resultado_dado === 5) {
+        $(".jugar").css("background-image", "url(img/5.jpg)");
+    }
+    if (resultado_dado === 6) {
+        $(".jugar").css("background-image", "url(img/6.jpg)");
+    }
+    return resultado_dado;
 }
 
 function avanzar() {
-    var valor_dado = lanzar_dado();
-    if (salida_juego === true) {
-        for (var i = 0, max = valor_dado; i < max; i++) {
-            total_avance = paso - 2;
-            posicion_ficha += total_avance;
-        }
-        $(".area_ficha").css("left", posicion_ficha);
+    if (acumulador_valor_dados >= 22) {
+        $(".jugar").css("background-image", "url()");
+        $(".msg").text('¡Felicidades culminaste el juego!');
+        clearTimeout();
     } else {
-        if (valor_dado === 6) {
-            alert('Usted sacó 6, puede lanzar para iniciar el juego');
-            salida_juego = true;
+        valor_dado = lanzar_dado();
+        if (salida_juego === true) {
+            for (var i = 0, max = valor_dado; i < max; i++) {
+                acumulador_valor_dados++;
+                if (acumulador_valor_dados <= 7) {
+                    total_avance_arriba = paso_ancho - 14.5;
+                    posicion_ficha_arriba += total_avance_arriba;
+                    $(".area_ficha").css("left", posicion_ficha_arriba);
+                    setTimeout(function () {
+                        $(".jugar").css("background-image", "url(img/dice.gif)");
+                        $(".msg").text("");
+                    }, 1600);
+                } else if (acumulador_valor_dados >= 8 && acumulador_valor_dados <= 11) {
+                    total_avance_derecha = paso_alto - 14.5;
+                    posicion_ficha_derecha += total_avance_derecha;
+                    $(".area_ficha").css("top", posicion_ficha_derecha);
+                    setTimeout(function () {
+                        $(".jugar").css("background-image", "url(img/dice.gif)");
+                        $(".msg").text("");
+                    }, 1600);
+                } else if (acumulador_valor_dados >= 12 && acumulador_valor_dados <= 18) {
+                    total_avance_abajo = paso_ancho - 14.5;
+                    posicion_ficha_abajo += total_avance_abajo;
+                    $(".area_ficha").css("left", "");
+                    $(".area_ficha").css("right", posicion_ficha_abajo);
+                    setTimeout(function () {
+                        $(".jugar").css("background-image", "url(img/dice.gif)");
+                        $(".msg").text("");
+                    }, 1600);
+                } else if (acumulador_valor_dados >= 19 && acumulador_valor_dados <= 22) {
+                    total_avance_izquierda = paso_alto - 14.5;
+                    posicion_ficha_izquierda += total_avance_izquierda;
+                    $(".area_ficha").css("top", "");
+                    $(".area_ficha").css("bottom", posicion_ficha_izquierda);
+                    if (acumulador_valor_dados >= 22) {
+                        $(".jugar").css("background-image", "url()");
+                        $(".msg").text('¡Felicidades culminaste el juego!');
+                        clearTimeout();
+                    } else {
+                        setTimeout(function () {
+                            $(".jugar").css("background-image", "url(img/dice.gif)");
+                            $(".msg").text("");
+                        }, 1000);
+                    }
+                }
+            }
         } else {
-            alert('Usted sacó ' + valor_dado + ', tiene que sacar 6 para iniciar el juego');
+            if (valor_dado === 6) {
+                $(".msg").text('¡Bien, sacaste 6, lanza de nuevo para avanzar tu ficha!');
+                salida_juego = true;
+                setTimeout(function () {
+                    $(".jugar").css("background-image", "url(img/dice.gif)");
+                    $(".msg").text("");
+                }, 1600);
+            } else {
+                $(".msg").text('¡Salió ' + valor_dado + ', reinta y saca 6 para iniciar la partida!');
+                setTimeout(function () {
+                    $(".jugar").css("background-image", "url(img/dice.gif)");
+                    $(".msg").text("");
+                }, 1600);
+            }
         }
     }
 }
--- index.php
+++ index.php
@@ -7,19 +7,49 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     </head>
     <body>
-        <div class="amarillo area_juego">
-            <div class="area_ficha efecto">
-                <div class="ficha"></div>
-            </div>
-            <?php
-            for ($index = 0; $index < 10; $index++) {
-                ?>
-                <div class="paso horizontal"></div>
-                <?php
-            }
-            ?>
-            <div class="jugar" onclick="avanzar()">Tirar dado</div>
-        </div>
+        <table cellspacing="0" cellpadding="0">
+            <tr>
+                <td class="mostasa paso">
+                    <div class="area_ficha efecto" style="left: 0; right: 0; bottom: 0; top: 0">
+                        <span class="ficha"></span>
+                    </div>
+                    <div><span>P/F</span></div>
+                </td>
+                <td class="crema"><div><span>1</span></div></td>
+                <td class="ladrillo"><div><span>2</span></div></td>
+                <td class="petroleo"><div><span>3</span></div></td>
+                <td class="mostasa"><div><span>4</span></div></td>
+                <td class="petroleo"><div><span>5</span></div></td>
+                <td class="ladrillo"><div><span>6</span></div></td>
+                <td class="mostasa"><div><span>7</span></div></td>
+            </tr>
+            <tr>
+                <td class="crema"><div><span>21</span></div></td>
+                <td colspan="6" rowspan="3">                    
+                    <div class="jugar" onclick="avanzar()"></div>
+                    <div class="msg"></div>
+                </td>
+                <td class="crema"><div><span>8</span></div></td>
+            </tr>
+            <tr>
+                <td class="ladrillo"><div><span>20</div></td>
+                <td class="ladrillo"><div><span>9</div></td>
+            </tr>
+            <tr>
+                <td class="petroleo"><div><span>19</span></div></td>
+                <td class="petroleo"><div><span>10</span></div></td>
+            </tr>
+            <tr>
+                <td class="ladrillo"><div><span>18</span></div></td>
+                <td class="mostasa"><div><span>17</span></div></td>
+                <td class="petroleo"><div><span>16</span></div></td>
+                <td class="crema"><div><span>15</span></div></td>
+                <td class="ladrillo"><div><span>14</span></div></td>
+                <td class="petroleo"><div><span>13</span></div></td>
+                <td class="naranja"><div><span>12</span></div></td>
+                <td class="mostasa"><div><span>11</span></div></td>
+            </tr>
+        </table>
         <script type="text/javascript" src="index.js"></script>
     </body>
 </html>
